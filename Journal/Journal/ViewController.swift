e909851bc81b77d95eb46030bef01c0bcbd9325b
LAURA JELENICH <laurajelenich@LAURAs-MBP-2.lan1>
2020-10-05 16:27:27 -0400

    first commit

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by LAURA JELENICH on 10/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
